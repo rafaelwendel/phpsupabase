2cd79ea0b3c446aa2af33e82c24ea2008765c09f
Rafael Wendel Pinheiro <rafaelwendel@hotmail.com>
2021-11-10 21:52:32 -0300

    Create the basic methods in PHPSupabase class

--- src/PHPSupabase.php
+++ src/PHPSupabase.php
@@ -3,5 +3,37 @@
 namespace PHPSupabase;
 
 class PHPSupabase {
-    
+    private $httpClient;
+    private $apiKey;
+    private $uriBase;
+    private $headers = [
+        'Content-Type' => 'application/json'
+    ];
+
+    public function __construct(string $apiKey, string $uriBase)
+    {
+        $this->apiKey = $apiKey;
+        $this->uriBase = $this->formatUriBase($uriBase);
+        $this->httpClient = new \GuzzleHttp\Client();
+
+        $this->headers['apikey'] = $this->apiKey;
+    }
+
+    private function formatUriBase(string $uriBase)
+    {
+        return (substr($uriBase , -1) == '/')
+            ? $uriBase
+            : $uriBase . '/'; 
+    }
+
+    public function getApiKey() : string
+    {
+        return $this->apiKey;
+    }
+
+    public function getUriBase($endPoint = '')
+    {
+        return $this->uriBase . $endPoint;
+    }
+
 }
