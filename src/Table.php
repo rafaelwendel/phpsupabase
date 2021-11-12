db4911b971b830dc8da2f787085cfa47851e7606
Rafael Wendel Pinheiro <rafaelwendel@hotmail.com>
2021-11-12 15:04:13 -0300

    Add defaultGetCall method in Table class

--- src/Table.php
+++ src/Table.php
@@ -6,10 +6,25 @@ class Table {
     private $service;
     private $tableName;
     private $bearerToken;
+    private $result;
 
     public function __construct(Service $service, string $tableName)
     {
         $this->service = $service;
         $this->tableName = $tableName;
+    }
+
+    public function getResult()
+    {
+        return $this->result;
+    }
+
+    private function defaultGetCall(string $queryString)
+    {
+        $uri = $this->service->getUriBase($this->tableName . '?' . $queryString);
+        $options = [
+            'headers' => $this->service->getHeaders()
+        ];
+        $this->result = $this->service->executeHttpRequest('GET', $uri, $options);
     }
 }
