1d63cbfd5bc2c57a2f5cdf75494aa385f055d7d9
Rafael Wendel Pinheiro <rafaelwendel@hotmail.com>
2021-11-17 16:55:37 -0300

    Add annotations in Service class methods

--- src/Service.php
+++ src/Service.php
@@ -12,6 +12,13 @@ class Service {
         'Content-Type' => 'application/json'
     ];
 
+    /**
+     * Construct method (Set the API key, URI base and instance GuzzleHttp client)
+     * @access public
+     * @param $apiKey String The Supabase project API Key
+     * @param $uriBase String API URI base (Ex: "https://abcdefgh.supabase.co/rest/v1/" OR "https://abcdefgh.supabase.co/auth/v1/") 
+     * @return void
+     */
     public function __construct(string $apiKey, string $uriBase)
     {
         $this->apiKey = $apiKey;
@@ -21,6 +28,12 @@ class Service {
         $this->headers['apikey'] = $this->apiKey;
     }
 
+    /**
+     * Format URI base with slash at end
+     * @access private
+     * @param $uriBase String API URI base (Ex: "https://abcdefgh.supabase.co/rest/v1/" OR "https://abcdefgh.supabase.co/auth/v1/") 
+     * @return void
+     */
     private function formatUriBase(string $uriBase) : string
     {
         return (substr($uriBase , -1) == '/')
@@ -28,51 +41,107 @@ class Service {
             : $uriBase . '/'; 
     }
 
+    /**
+     * Returns the API key
+     * @access public
+     * @return string
+     */
     public function getApiKey() : string
     {
         return $this->apiKey;
     }
 
+    /**
+     * Returns the URI base
+     * @access public
+     * @param $endPoint (optional) String The end point to concatenate to URI base
+     * @return string
+     */
     public function getUriBase(string $endPoint = '') : string
     {
         return $this->uriBase . $endPoint;
     }
 
+    /**
+     * Returns the HTTP Client (GuzzleHttp)
+     * @access public
+     * @return \GuzzleHttp\Client
+     */
     public function getHttpClient() : \GuzzleHttp\Client
     {
         return $this->httpClient;
     }
 
+    /**
+     * Set a header to be use in the API request
+     * @access public
+     * @param $header String The header key to be set
+     * @param $value String The value of header
+     * @return void
+     */
     public function setHeader(string $header, string $value) : void
     {
         $this->headers[$header] = $value;
     }
 
+    /**
+     * Returns the set headers
+     * @access public
+     * @return array
+     */
     public function getHeaders() : array
     {
         return $this->headers;
     }
 
+    /**
+     * Returns the error generated
+     * @access public
+     * @return string
+     */
     public function getError() : string
     {
         return $this->error;
     }
 
+    /**
+     * Returns a new instance of Auth class
+     * @access public
+     * @return Auth
+     */
     public function createAuth() : Auth
     {
         return new Auth($this);
     }
 
+    /**
+     * Returns a new instance of Database class
+     * @access public
+     * @param $tableName String The table to be used
+     * @param $primaryKey (optional) String The table primary key (usually "id")
+     * @return Database
+     */
     public function initializeDatabase(string $tableName, string $primaryKey = 'id') : Database
     {
         return new Database($this, $tableName, $primaryKey);
     }
 
+    /**
+     * Returns a new instance of QueryBuilder class
+     * @access public
+     * @return QueryBuilder
+     */
     public function initializeQueryBuilder() : QueryBuilder
     {
         return new QueryBuilder($this);
     }
 
+    /**
+     * Format the exception thrown by GuzzleHttp, formatting the error message
+     * @access public
+     * @param $e \GuzzleHttp\Exception\RequestException The exception thrown by GuzzleHttp
+     * @return void
+     */
     public function formatRequestException(\GuzzleHttp\Exception\RequestException $e) : void
     {
         if($e->hasResponse()){
@@ -88,6 +157,14 @@ class Service {
         }
     }
 
+    /**
+     * Execute a Http request in Supabase API
+     * @access public
+     * @param $method String The request method (GET, POST, PUT, DELETE, PATCH, ...)
+     * @param $uri String The URI to be requested (including the endpoint)
+     * @param $options array Requisition options (header, body, ...) 
+     * @return array
+     */
     public function executeHttpRequest(string $method, string $uri, array $options) : array
     {
         try{
